58dffcf3a97acdd716f7d10eb48813c92712d639
Minhtuan25022002 <ntmt52502@gmail.com>
2023-12-16 12:46:22 +0700

    create user, check eligibility

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,9 +1,34 @@
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const User = require('../models/UserModel')
+
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser
         try {
-            resolve({})
-        } catch (error) {
-            reject(error)
+            const checkUser = await User.findOne({
+                email: email,
+            })
+            if (checkUser !== null) {
+                resolve({
+                    status: 'OK',
+                    message: 'The email is already in the database'
+                })
+            }
+            const createdUser = await User.create({
+                name,
+                email,
+                password,
+                confirmPassword,
+                phone
+            });
+            if (createdUser) {
+                resolve({
+                    status: 'OK',
+                    message: 'SUCCESS',
+                    data: createdUser
+                })
+            };
+        } catch (e) {
+            reject(e)
         }
     })
 }
@@ -11,3 +36,5 @@ const createUser = () => {
 module.exports = {
     createUser
 }
+
+//khi tách controller ra services như này Nó sẽ giúp mình phân tách code ra, để sau này dự án có lớn lên thì sẽ dễ quản lí
